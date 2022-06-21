3d45294806239d8f52624d73cd8ce9b8490f6002
Dania Estrada <daniaestrada@Danias-MBP.domain>
2022-06-20 18:35:49 -0600

    first commit

---
+++ src/main/java/com/app/stocktrac/StocktracApplication.java
@@ -0,0 +1,13 @@
+package com.app.stocktrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/stocktrac/StocktracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.stocktrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
